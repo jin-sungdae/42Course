07e9466400d8e39bcecab4ce1d1e949dd7ec7dde
Sungdae Jin <sjin@c6r8s5.42seoul.kr>
2022-02-24 14:25:07 +0900

    gnl 추가

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sjin <sjin@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/13 10:57:27 by sjin              #+#    #+#             */
+/*   Updated: 2021/01/13 10:57:30 by sjin             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+
+# define OPEN_MAX 32
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5000
+# endif
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strdup(char *str);
+
+#endif
