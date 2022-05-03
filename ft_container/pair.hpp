946b8fe53d130fa31f2eb07f5e76f2e4b70cb597
Sungdae Jin <sjin@c4r1s3.42seoul.kr>
2022-05-03 15:34:53 +0900

    corona

--- ft_container/pair.hpp
+++ ft_container/pair.hpp
@@ -9,6 +9,8 @@ namespace ft{
         typedef T2      second_type;
         first_type      first;
         second_type     second;
+        template<class U, class V>
+            pair(const pair<U, V> &pr);
         pair(const pair& one, const pair& two);
         pair& operator=(const pair& p);
         pair();
@@ -19,6 +21,10 @@ namespace ft{
 
     template<class T1, class T2>
     pair<T1, T2>::pair(const pair&a, const pair& two) :first(a) , second(two){}
+
+    template<class T1, class T2>
+    template<class U, class V>
+    pair<T1, T2>::pair(const pair<U, V> &pr) : first(pr.first), second(pr.second) {}
 
     template<class T1, class T2>
     pair<T1, T2>& pair<T1, T2>::operator=(const pair& p){
