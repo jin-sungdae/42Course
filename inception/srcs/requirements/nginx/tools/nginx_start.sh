31d63556328e9b68240f9966dcdf7a72e576c97a
jin-sungdae <makonle@naver.com>
2022-06-30 21:50:13 +0900

    Fix: inception nginx 443port

--- inception/srcs/requirements/nginx/tools/nginx_start.sh
+++ inception/srcs/requirements/nginx/tools/nginx_start.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/ssl/certs/nginx.crt ]; then
 echo "Nginx: setting up ssl ...";
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=sjin.42.fr";
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -keyout /etc/ssl/private/nginx.key -out /etc/ssl/certs/nginx.crt -subj "/C=KR/ST=Seoul/L=Seoul/O=wordpress/CN=sjin.42.kr";
 echo "Nginx: ssl is set up!";
 fi
 
