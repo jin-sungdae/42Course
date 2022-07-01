4751ff22aee04a54d1e5444fd5353ee349efcf31
jin-sungdae <makonle@naver.com>
2022-07-01 14:23:51 +0900

    Fix: inception mariadb 수정21

--- inception/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -15,10 +15,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html;
 	wp core download --allow-root;
 	mv /var/www/wp-config.php /var/www/html/
-	# echo "Wordpress: creating users..."
-	# wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
-	# wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
-	# echo "Wordpress: set up!"
+	echo "Wordpress: creating users..."
+	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
+	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
+	echo "Wordpress: set up!"
 fi
 
 exec "$@"
