d916f44b417e65a26f2f4f5496fad017ef7d794f
jin-sungdae <makonle@naver.com>
2022-07-01 15:34:15 +0900

    ADD: inception run.sh 설치

---
+++ inception/run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt-get install make
+
+## docker 설치
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+
+## docker-compose 설치
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+
+
+
+
