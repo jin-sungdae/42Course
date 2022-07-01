87e5059abc7c9ec5555965e2e539d21c42710f25
jin-sungdae <makonle@naver.com>
2022-07-01 15:36:04 +0900

    ADD: inception run.sh 설치

--- inception/run.sh
+++ inception/run.sh
@@ -4,7 +4,7 @@ apt-get install make
 
 ## docker 설치
 apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt-cache policy docker-ce
