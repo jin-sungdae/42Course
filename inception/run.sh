3865f14a22ba2ab32d151b5f74472318f08442a8
jin-sungdae <makonle@naver.com>
2022-07-04 22:03:03 +0900

    inception conf

--- inception/run.sh
+++ inception/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-apt-get install make
+apt-get install -y make
+apt-get install -y vim
+
+## vi /etc/resolv.conf 수정
+echo "search 8.8.4.4\nnameserver 8.8.8.8" > resolv.conf
+rm -rf /etc/resolv.conf
+mv resolv.conf /etc
+
+echo "127.0.0.1 $sjin.42.fr" >> /etc/hosts
 
 ## docker 설치
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
-apt-cache policy docker-ce
-apt install docker-ce
+apt install apt-transport-https ca-certificates curl software-properties-common gnupg2
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce
 
 ## docker-compose 설치
 curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
