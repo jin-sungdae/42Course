a6e4db844fb12531e0a38f60c36f3e8666ef85aa
jin-sungdae <makonle@naver.com>
2022-07-05 17:53:39 +0900

    inception 최종..1

--- inception/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-	sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
-	chown -R www-data:www-data /var/www/*;
-	chown -R 755 /var/www/*;
-	mkdir -p /run/php/;
-	touch /run/php/php7.3-fpm.pid;
+set -eux;
+
+sed -i "s/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/" "/etc/php/7.3/fpm/pool.d/www.conf";
+chown -R www-data:www-data /var/www/*;
+chown -R 755 /var/www/*;
+mkdir -p /run/php/;
+touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
 	echo "Wordpress: setting up..."
