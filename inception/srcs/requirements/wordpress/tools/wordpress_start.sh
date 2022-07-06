448413a43f3446fc128551e1902980819e9abf59
jin-sungdae <makonle@naver.com>
2022-07-06 21:57:37 +0900

    inception 최종..2

--- inception/srcs/requirements/wordpress/tools/wordpress_start.sh
+++ inception/srcs/requirements/wordpress/tools/wordpress_start.sh
@@ -9,7 +9,7 @@ mkdir -p /run/php/;
 touch /run/php/php7.3-fpm.pid;
 
 if [ ! -f /var/www/html/wp-config.php ]; then
-	echo "Wordpress: setting up..."
+	echo "---------------------Wordpress: setting------------------"
 	mkdir -p /var/www/html
 	wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar;
 	chmod +x wp-cli.phar; 
@@ -17,10 +17,10 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	cd /var/www/html;
 	wp core download --allow-root;
 	mv /var/www/wp-config.php /var/www/html/
-	echo "Wordpress: creating users..."
+	echo "---------------------Wordpress: creating users--------------"
 	wp core install --allow-root --url=${WP_URL} --title=${WP_TITLE} --admin_user=${WP_ADMIN_LOGIN} --admin_password=${WP_ADMIN_PASSWORD} --admin_email=${WP_ADMIN_EMAIL}
 	wp user create --allow-root ${WP_USER_LOGIN} ${WP_USER_EMAIL} --user_pass=${WP_USER_PASSWORD};
-	echo "Wordpress: set up!"
+	echo "---------------------Wordpress: setting finish--------------"
 fi
 
 exec "$@"
